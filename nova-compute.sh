6eb9049334bf8933476097c00f4b81c13832f38e
Kiall Mac Innes <kiall@managedit.ie>
2011-11-17 20:43:05 +0000

    Update nova-compute.sh

--- nova-compute.sh
+++ nova-compute.sh
@@ -28,5 +28,7 @@ cp nova.conf api-paste-keystone.ini /etc/nova/
 chown nova:nova /etc/nova/nova.conf /etc/nova/api-paste-keystone.ini
 
 service nova-api restart
-service nova-network start
-service nova-compute start
+service nova-network restart
+service nova-compute restart
+
+echo "*** MANUALLY RESTART ALL NOVA SERVICES ***"
