24d5e8ed7e6abf49382606121c81bb11b5da3c90
Kiall Mac Innes <kiall@managedit.ie>
2011-11-20 21:13:08 +0000

    Switch to using the qcow2 oneiric image

--- glance-upload-oneiric.sh
+++ glance-upload-oneiric.sh
@@ -3,19 +3,12 @@
 # Import Settings
 . settings
 
-if [ ! -f "oneiric-server-cloudimg-amd64.tar.gz" ] ; then
+if [ ! -f "oneiric-server-cloudimg-amd64-disk1.img" ] ; then
 	echo "Downloading image"
-	wget http://cloud-images.ubuntu.com/oneiric/current/oneiric-server-cloudimg-amd64.tar.gz
-fi
-
-if [ ! -f "oneiric-server-cloudimg-amd64.img" ] ; then
-        echo "Extracting image"
-	tar xfzv oneiric-server-cloudimg-amd64.tar.gz
+	wget http://cloud-images.ubuntu.com/oneiric/current/oneiric-server-cloudimg-amd64-disk1.img
 fi
 
 TOKEN=`./obtain-token.sh`
-echo "Uploading kernel"
-glance -A $TOKEN add name="ubuntu-oneiric-kernel" is_public=true container_format=aki disk_format=aki < oneiric-server-cloudimg-amd64-vmlinuz-virtual
 
 echo "Uploading image"
-glance -A $TOKEN add name="ubuntu-oneiric" is_public=true container_format=ami kernel_id=1 disk_format=ami < oneiric-server-cloudimg-amd64.img
+glance -A $TOKEN add name="ubuntu-oneiric" is_public=true container_format=ovf disk_format=qcow2 < oneiric-server-cloudimg-amd64-disk1.img
