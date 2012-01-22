b54477e9b5c3403500bd797fb3c595c12ea8212a
Kiall Mac Innes <kiall@managedit.ie>
2012-01-22 15:28:45 +0000

    Add support for specifying libvirt type.

--- nova-compute.sh
+++ nova-compute.sh
@@ -5,6 +5,17 @@
 
 apt-get install -y nova-api nova-compute nova-network python-keystone python-mysqldb mysql-client curl
 
+# Check for kvm (hardware based virtualization).  If unable to initialize
+# kvm, we drop back to the slower emulation mode (qemu).  Note: many systems
+# come with hardware virtualization disabled in BIOS.
+if [[ "$LIBVIRT_TYPE" == "kvm" ]]; then
+    modprobe kvm || true
+    if [ ! -e /dev/kvm ]; then
+        echo "WARNING: Switching to QEMU"
+        LIBVIRT_TYPE=qemu
+    fi
+fi
+
 # Nova Setup
 sed -e "s,999888777666,$SERVICE_TOKEN,g" api-paste-keystone.ini.tmpl > api-paste-keystone.ini
 
@@ -18,6 +29,7 @@ sed -e "s,%MYSQL_NOVA_PASS%,$MYSQL_NOVA_PASS,g" -i nova.conf
 sed -e "s,%FIXED_RANGE_MASK%,$FIXED_RANGE_MASK,g" -i nova.conf
 sed -e "s,%FIXED_RANGE_NET%,$FIXED_RANGE_NET,g" -i nova.conf
 sed -e "s,%FIXED_RANGE%,$FIXED_RANGE,g" -i nova.conf
+sed -e "s,%LIBVIRT_TYPE%,$LIBVIRT_TYPE,g" -i nova.conf
 
 cp nova.conf api-paste-keystone.ini /etc/nova/
 chown nova:nova /etc/nova/nova.conf /etc/nova/api-paste-keystone.ini
